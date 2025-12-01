d4544bc71f4daaeda3da4aec05e6b2c83a880a5b
Avneesh Sarwate <avneeshsarwate@gmail.com>
2025-12-01 00:11:18 -0500

    first commit

---
+++ webview-ui/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '', // important – no leading slash
+  build: {
+    outDir: '../media',
+    emptyOutDir: true
+  }
+})
